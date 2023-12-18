c12dc3bd1b5865441eb20148b8724dc9193ea8c3
julio cesar <jcmcf2012@gmail.com>
2023-12-18 20:40:37 -0300

    fix: local storage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,60 @@
-import React, { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [nome, setNome] = useState("");
+  const [nomes, setNomes] = useState([]);
 
-  const [nome, setNome] = useState('')
+  // 1 - Pegar informação quando dar um refresh na tela. Sol: useEffect
+  // 2 - Disponibilizar informações no html(reactjs)
+
+  // Roda o bloco de código a partir da mudança de alguma variável
+  useEffect(() => {
+    const nomesLocalStorage = JSON.parse(
+      localStorage.getItem("searchArmazenar")
+    );
+
+    if (nomesLocalStorage?.length > 0) setNomes(nomesLocalStorage);
+  }, []);
+
+  useEffect(() => {
+    if (nomes?.length > 0)
+      localStorage.setItem("searchArmazenar", JSON.stringify(nomes));
+  }, [nomes]);
 
   function armazenar(valor) {
-    let searchArmazenarArray = JSON.parse(
-      localStorage.getItem('searchArmazenar') || '[]'
-    )
     let searchArmazenar = {
-      nome: valor
-    }
+      nome: valor,
+    };
+
     if (valor.length != 0) {
-      searchArmazenarArray.push(searchArmazenar)
-
-      document.getElementById("conteudo").insertAdjacentHTML('beforeend', "Nome: " + valor + "<br/><hr/>")
-
+      setNomes((prev) => {
+        return [...prev, searchArmazenar];
+      });
     }
-    localStorage.setItem('searchArmazenar', JSON.stringify(searchArmazenarArray))
   }
-  // function consultar(valor) {
-  //   alert(localStorage.getItem(valor))
-  // }
-
-  // function apagar(valor) {
-  //   localStorage.removeItem(valor)
-  // }
 
   return (
     <>
       <label>Digitar Nome </label>
-      <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
-      <button onClick={() => armazenar(nome)}>Gravar</button> <br /><br />
+      <input
+        type="text"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+      <button onClick={() => armazenar(nome)}>Gravar</button> <br />
+      <br />
       {/* <button onClick={() => consultar('ls_nome')}>Consultar</button>
       <button onClick={() => apagar('ls_nome')}>Remover</button> <br /><br /><br /> */}
-
-      <span id='conteudo'> </span>
-
+      <span id="conteudo"> </span>
+      <span id="conteudo">
+        {nomes?.length > 0 &&
+          nomes?.map((item, index) => {
+            return <h1 key={index}>{item.nome}</h1>;
+          })}
+      </span>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
